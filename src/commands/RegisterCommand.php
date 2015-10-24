8c73493a294f201029b2aa111f5633b031d6e75f
Vitor Mattos <vitor.mattos@phprio.org>
2015-10-24 13:21:34 -0200

    test

--- src/commands/RegisterCommand.php
+++ src/commands/RegisterCommand.php
@@ -35,7 +35,7 @@ class RegisterCommand extends Command
             $profile = json_decode(file_get_contents('https://www.duolingo.com/users/' . $arguments));
             if ($profile) {
                 $update = $this->telegram->getWebhookUpdates()->all();
-                $this->replyWithMessage($update,true);
+                $this->replyWithMessage(print_r($update,true));
 return;
                 $db = DB::getInstance();
                 try {
