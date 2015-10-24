10b6db5cc7fce93f2688ac980dcf7f9c1fe7cd89
Vitor Mattos <vitor.mattos@phprio.org>
2015-10-24 13:51:39 -0200

    list

---
+++ src/commands/ListUsersCommand.php
@@ -0,0 +1,46 @@
+<?php
+namespace Commands;
+
+use Telegram\Bot\Commands\Command;
+use Telegram\Bot\Actions;
+use Base\DB;
+
+class ListCommand extends Command
+{
+
+    /**
+     *
+     * @var string Command Name
+     */
+    protected $name = "list";
+
+    /**
+     *
+     * @var string Command Description
+     */
+    protected $description = "List duolingo usernames in this group";
+
+    /**
+     * @inheritdoc
+     */
+    public function handle($arguments)
+    {
+        // This will update the chat status to typing...
+        $this->replyWithChatAction(Actions::TYPING);
+
+        $update = $this->telegram->getWebhookUpdates()->all();
+
+        $db = DB::getInstance();
+        try {
+            $stmt = $db->perform("SELECT username FROM users WHERE chat_id = :chat_id;", [
+                'chat_id' => $update['message']['chat']['id']
+            ]);
+            $this->replyWithMessage(print_r($db->fetchAll($stmt), true));
+        } catch (\Exception $e) {
+            if($update['message']['from']['id'] == 37900977) {
+                $this->replyWithMessage(print_r($e->getMessage(),true));
+            }
+            $this->replyWithMessage('Error listing users');
+        }
+    }
+}
